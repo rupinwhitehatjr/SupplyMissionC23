607116a8e0b03f5ab632fffcca0765d3ca7f768e
AayushBindal <73348182+AayushBindal@users.noreply.github.com>
2020-12-10 10:51:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,43 +12,40 @@ function preload()
 }
 
 function setup() {
+	// to create the canvas 
 	createCanvas(800, 700);
-	rectMode(CENTER);
-	
-
+	// have rect mode to center 
+	rectMode(CENTER);	
+	//create a package sprite 
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
-
+	// create a helicopter sprite
 	helicopterSprite=createSprite(width/2, 200, 10,10);
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6
-
+	// create a helicopter sprite 
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
-
-
+	// create engine and world 
 	engine = Engine.create();
 	world = engine.world;
-
+	// create a package sprite and world 
 	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
 	World.add(world, packageBody);
-	
-
-	//Create a Ground
+	//create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
-
+	// have the box position 
  	boxPosition=width/2-100
  	boxY=610;
-
-
+	// 
  	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
  	boxleftSprite.shapeColor=color(255,0,0);
-
+	// 
  	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
  	World.add(world, boxLeftBody);
-
+	
  	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
  	boxBase.shapeColor=color(255,0,0);
 
@@ -60,45 +57,18 @@ function setup() {
 
  	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
  	World.add(world, boxRightBody);
-
-
 	Engine.run(engine);
-  
 }
-
-
 function draw() {
   rectMode(CENTER);
   background(0);
- 
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
-
-  
+  keyPressed();
   drawSprites();
-  
-  
- 
 }
-
 function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-
-    helicopterSprite.x=helicopterSprite.x-20;    
-    translation={x:-20,y:0}
-    Matter.Body.translate(packageBody, translation)
-
-
-  } else if (keyCode === RIGHT_ARROW) {
-    helicopterSprite.x=helicopterSprite.x+20;
-    translation={x:20,y:0}
-    Matter.Body.translate(packageBody, translation)
-  }
-  else if (keyCode === DOWN_ARROW) {
+ if (keyCode === DOWN_ARROW) {
     Matter.Body.setStatic(packageBody,false);
-    
   }
 }
-
-
-
